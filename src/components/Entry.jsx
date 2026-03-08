ea8a548e7eb715f0429d67fe00114b5b7df96a64
Filip Szkarlat Szczepaniak <filip.szkarlat@gmail.com>
2026-03-09 00:06:28 +0100

    working on the Entry component

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,14 +1,19 @@
-import path from "path"
+import { HiMapPin } from "react-icons/hi2";
 
 export default function Entry(props) {
     console.log(props)
     return (
         <div className="entry">
-        //     <img src={props.img.src} alt={props.img.alt} />
-        //     <div className="entry--details">
-        //         <img src={path} alt="path icon" />
-        //         <span>{props.country}</span>
-        //     </div>
+            <div className='image-container'>
+                <img src={props.img.src} alt={props.img.alt} />
+            </div>
+            <div className="entry--details">
+                <HiMapPin />
+                <span className="country">{props.country}</span>
+                <a href={props.googleMapsLink} target="_blank" rel="noopener noreferrer">
+                    View on Google Maps
+                </a>
+            </div>
          </div>
     )
 }
